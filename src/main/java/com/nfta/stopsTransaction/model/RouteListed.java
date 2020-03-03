39446b8ad17f67a742644eeb6adb3080948de93e
aditya0335 <bansaladitya0335@gmail.com>
2020-03-03 03:19:26 -0500

    model 1

---
+++ src/main/java/com/nfta/stopsTransaction/model/RouteListed.java
@@ -0,0 +1,24 @@
+package com.nfta.stopsTransaction.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+//@Entity
+//@Data
+//@Table(name = "route_listed")
+public class RouteListed {
+//	@Id
+//	@OneToOne(mappedBy = "Route")
+//	@JoinColumn(name = "route_id")
+//	private Route route;
+//	@Id
+//	@OneToOne(mappedBy = "stop_transactions")
+//	@JoinColumn(name = "request_id")
+//	private StopTransactions stopTransactions;
+
+}
