d0171f68cf53e6e8aaa8abfefd35893a3875e7a5
aditya0335 <bansaladitya0335@gmail.com>
2020-03-26 18:44:31 -0400

    Changes in user controller

--- src/main/java/com/nfta/stopsTransaction/controller/UserController.java
+++ src/main/java/com/nfta/stopsTransaction/controller/UserController.java
@@ -2,6 +2,7 @@ package com.nfta.stopsTransaction.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,20 @@ public class UserController {
 		}
 		return s;
 	}
+	
+//	@RequestMapping(value = "/deleteUser/{user_id}", method = RequestMethod.DELETE)
+//	public @ResponseBody String deleteUser(@PathVariable int user_id) {
+//
+//		String s="";
+//		try {
+//			s=adminService.addUser(adminUser);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return s;
+//	}
+	
+	
 
 	
 	
