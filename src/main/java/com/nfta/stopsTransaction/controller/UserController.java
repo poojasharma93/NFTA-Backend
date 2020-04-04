eddc34cb8a2bd7253fc73fb8ad6df8d939fb80b8
Anuj Bhargava <anujbhargava7@Anujs-MBP.lan1>
2020-04-03 22:20:52 -0400

    controller refactor

--- src/main/java/com/nfta/stopsTransaction/controller/UserController.java
+++ src/main/java/com/nfta/stopsTransaction/controller/UserController.java
@@ -1,63 +1,41 @@
 package com.nfta.stopsTransaction.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nfta.stopsTransaction.config.JwtTokenUtil;
-import com.nfta.stopsTransaction.config.JwtUserDetailsService;
-import com.nfta.stopsTransaction.model.JwtRequest;
-import com.nfta.stopsTransaction.model.JwtResponse;
-import com.nfta.stopsTransaction.model.UserDTO;
+import com.google.gson.Gson;
+import com.nfta.stopsTransaction.model.AdminUser;
+import com.nfta.stopsTransaction.model.StopTransactions;
+import com.nfta.stopsTransaction.service.AdminService;
+
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins="http://localhost:3000")
+//@CrossOrigin(origins="https://nfta-web.herokuapp.com")
 public class UserController {
-
+	
 	@Autowired
-	private AuthenticationManager authenticationManager;
+	AdminService adminService;
+	
+	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
+	public @ResponseBody String addUser(@RequestBody AdminUser adminUser) {
 
-	@Autowired
-	private JwtTokenUtil jwtTokenUtil;
-
-	@Autowired
-	private JwtUserDetailsService userDetailsService;
-
-	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
-
-		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-
-		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-
-		final String token = jwtTokenUtil.generateToken(userDetails);
-
-		return ResponseEntity.ok(new JwtResponse(token));
-	}
-
-	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
-		return ResponseEntity.ok(userDetailsService.save(user));
-	}
-
-	private void authenticate(String username, String password) throws Exception {
+		String s="";
 		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
-		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
+			s=adminService.addUser(adminUser);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		return s;
 	}
 }
-
-//TODO logout
